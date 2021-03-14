93e7a1c1f00f0f3bbaa0fe3020242ebddd70c1f3
SneakyWizard <bluna_work@yahoo.com>
2021-03-14 10:23:00 -0700

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -13,10 +13,10 @@ const req_from_str = require('require-from-string');
 	Usage: 
 
 		if ( !global.objects ) {
-			var Objects = require('/home/perladm/node-apps/lib/Objects').Objects;
+			var Objects = require('/home/xxx/node-apps/lib/Objects').Objects;
 			Objects     = new Objects();
 
-			Objects.init( { path: '/home/perladm/node-apps/lib' } );
+			Objects.init( { path: '/home/xxx/node-apps/lib' } );
 			console.log( global.objects );
 		}
 
